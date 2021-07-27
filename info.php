2f5ab163ff910d4da4d3a9ef050ee7d551418643
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2021-07-27 21:00:06 +1000

    Initial commit - Nugetzrul3

---
+++ info.php
@@ -0,0 +1,129 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use DOGECModule\DOGECModule;
+
+$dogec = new DOGECModule();
+
+if (!array_key_exists("address", $_GET) and array_key_exists("rate", $_GET)) {
+    if (!isset($_GET['rate']) or $_GET['rate'] == '') {
+        echo json_encode([
+            "result" => "",
+            "error" => "Rate has not been passed"
+        ]);
+        return;
+    }
+    $currency = strtolower($_GET['rate']);
+    $price = $dogec->getPrice($currency);
+
+    echo json_encode([
+        "result" => $price,
+        "error" => ""
+    ]);
+}
+else if (!array_key_exists("rate", $_GET) and array_key_exists("address", $_GET)) {
+    if (!isset($_GET['address']) or $_GET['address'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Address has not been passed :("
+        ]);
+        return;
+    }
+    else if (!isset($_GET['amount']) or $_GET['amount'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Amount value has not been passed :("
+        ]);
+        return;
+    }
+    else if (!isset($_GET['otime']) or $_GET['otime'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Timestamp Value has not been passed :("
+        ]);
+        return;
+    }
+    else if (!isset($_GET['tx']) or $_GET['tx'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Transaction has not been passed :("
+        ]);
+        return;
+    }
+    else if (!isset($_GET['conf']) or $_GET['conf'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Min Confirmations has not been passed :("
+        ]);
+        return;
+    }
+    else if (!isset($_GET['mtime']) or $_GET['mtime'] == '') {
+        echo json_encode([
+            "status" => "invalid",
+            "error" => "Max time not has not been passed :("
+        ]);
+        return;
+    }
+
+    $address = $_GET['address'];
+    $amount = $_GET['amount'];
+    $timestamp = $_GET['otime'];
+    $txid = $_GET['tx'];
+    $mconf = $_GET['conf'];
+    $max_time = $_GET['mtime'];
+    $current_timestamp = time();
+
+    if ($current_timestamp > $timestamp + ($max_time * 60)) {
+        echo json_encode([
+            "status" => "expired",
+        ]);
+        return;
+    }
+
+    $response = $dogec->existsTransaction($address, $amount, $timestamp);
+    if (!$response) {
+        echo json_encode([
+            "status" => "failed"
+        ]);
+        return;
+    }
+    if ($response['exists']) {
+        $trasaction = $response['txid'];
+        $confirmations = $dogec->checkConfirmations($trasaction);
+
+        if ($confirmations >= $mconf) {
+            echo json_encode([
+                "status" => "confirmed",
+                "message" => "Transaction successfully confirmed.",
+                "transaction_id" => $trasaction,
+                "confirmations" => $confirmations
+            ]);
+            return;
+        }
+        else {
+            echo json_encode([
+                "status" => "detected",
+                "message" => "Transaction detected. Waiting for confirmations - "
+                . $confirmations . "/" . $mconf,
+                "transaction_id" => $trasaction,
+                "confirmations" => $confirmations
+            ]);
+            return;
+        }
+    }
+    else {
+        echo json_encode([
+            "status" => "waiting",
+            "message" => "Waiting for payment..."
+        ]);
+        return;
+    }
+}
+else {
+    echo json_encode([
+        "result" => "",
+        "error" => "Invalid Request"
+    ]);
+}
+?>
---
+++ lib/DOGECModule.php
@@ -0,0 +1,143 @@
+<?php 
+
+namespace DOGECModule;
+
+use GuzzleHttp\Client;
+
+class DOGECModule{
+    
+    public function __construct()
+    {
+        $this->explorer_url = "https://dogec.flitswallet.app/api/v1/";
+		$this->price_url = "https://api.coingecko.com/api/v3/simple/price?ids=dogecash&vs_currencies=";
+        $this->client = new client();
+    }
+
+    //
+    // Look in the explorer if a newer transaction than $timestap in ms (int) with $amount (Float) exists for $address (varchar)
+    //
+    public function existsTransaction($address, $amount, $timestamp)
+    {
+        try{
+            $transactions = $this->getAddressTransactions($address);
+
+            foreach($transactions as $transaction)
+            {
+                $transaction_info = $this->getTransaction($transaction);
+
+                //allowing only unconfirmed transactions & confirmed transactions newer than $timestamp
+                if($transaction_info['blocktime'] != 0 && $transaction_info['blocktime'] < $timestamp)
+                {
+                    //transaction doesn't exist
+                    return [
+                        'exists' => false,
+                        'txid' => ""
+                    ];
+                }
+
+                foreach($transaction_info['vout'] as $vout)
+                {
+                    if($vout['value'] == $amount && $vout['scriptPubKey']['addresses'][0] == $address)
+                    {
+                        return [
+                            'exists' => true,
+                            'txid' => $transaction
+                        ];
+                    }
+                }
+            }
+
+        }
+        catch(\Throwable $e)
+        {
+            return false;
+        }
+        
+    }
+
+    //
+    // Check how many confirmations does $txid have (varchar)
+    //
+    public function checkConfirmations($txid)
+    {
+        try{
+            $transaction = $this->getTransaction($txid);
+            return $transaction['confirmations'];
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get transactions from address
+    //
+    public function getAddressTransactions($address)
+    {
+        try{
+            $addressEndpoint = $this->explorer_url . "address/$address";            
+            $transactions = $this->client->request('GET', $addressEndpoint);
+
+            //convert response into array
+            $transactions_array = (json_decode($transactions->getBody()->getContents(), true))['transactions'];
+            return $transactions_array;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get trasnsaction
+    //
+    public function getTransaction($txid)
+    {
+        try{
+            $transactionEndpoint = $this->explorer_url . "tx/$txid"; 
+            $transaction = $this->client->request('GET', $transactionEndpoint);
+
+            //convert response into array
+            $transaction = json_decode($transaction->getBody()->getContents(), true);
+
+            return $transaction;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+	
+	//
+	//Check Address
+	//
+	public function checkAddress($address) 
+	{
+		$addressEndpoint = $this->explorer_url . "address/$address";
+		try {
+			$address = $this->client->request('GET', $addressEndpoint);
+			return true;
+		}
+		catch (\Throwable $e) {
+			return false;
+		}
+	}
+	
+	//
+	//Get currency price
+	//
+	public function getPrice($currency)
+	{
+		try {
+			$priceEndpoint = $this->price_url . $currency;
+			$price = $this->client->request('GET', $priceEndpoint);
+			
+			$price = json_decode($price->getBody()->getContents(), true);
+			
+			return $price['dogecash'][$currency];
+		}
+		catch (\Throwable $e) {
+			throw $e;
+		}
+
+	}
+
+}
