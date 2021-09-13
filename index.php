a315281674f5cf113c3debfea62ca5e4bc4ac1e0
Weoka <weokap@gmail.com>
2021-09-13 18:16:49 +0200

    [Cors] Allow origin

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
 require_once('vendor/autoload.php');
 
 use DOGECModule\DOGECModule;
@@ -126,4 +128,3 @@ else {
         "error" => "Invalid Request"
     ]);
 }
-?>
